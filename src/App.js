31cab2f938fb2fa28fd225c5dbf01e7bae3c83e8
rashed <rashedfl123@gmail.com>
2020-09-06 14:15:23 +0600

    weather range and form add

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import 'weather-icons/css/weather-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Weather from './component/weather.component';
 import { render } from '@testing-library/react';
+import Form from './component/form.component';
 
 //api.openweathermap.org/data/2.5/weather?q=London,uk
 const API_key = "805ea0e0782759c817dd9e07172d332e";
@@ -27,13 +28,47 @@ class App extends React.Component{
     };
     this.getWeather();
     this.weatherIcon = {
-      Thunderstrom:"wi-thunderstorm"
+      Thunderstorm:"wi-thunderstorm",
+      Drizzle:"wi-sleet",
+      Rain:"wi-storm-showers",
+      Snow:"wi-snow",
+      Atmosphere:"wi-fog",
+      Clear:"wi-day-sunny",
+      Clouds:"wi-day-fog"
     };
   }
 
   calCelsius(temp){
     let cell = Math.floor(temp-273.15);
     return cell;
+  }
+
+  get_WeatherIcon(icons,rangeID){
+    switch(true){
+      case rangeID>=200 && rangeID <= 232:
+        this.setState({icon:this.weatherIcon.Thunderstorm});
+        break;
+      case rangeID>=300 && rangeID <= 321:
+        this.setState({icon:this.weatherIcon.Drizzle});
+        break;
+      case rangeID>=500 && rangeID <= 531:
+        this.setState({icon:this.weatherIcon.Rain});
+        break;
+      case rangeID>=600 && rangeID <= 622:
+        this.setState({icon:this.weatherIcon.Snow});
+        break;
+      case rangeID>=701 && rangeID <= 781:
+        this.setState({icon:this.weatherIcon.Atmosphere});
+        break;
+      case rangeID===800:
+        this.setState({icon:this.weatherIcon.Clear});
+        break;
+      case rangeID>=801 && rangeID <= 804:
+        this.setState({icon:this.weatherIcon.Clouds});
+        break;
+        default:
+          this.setState({icon:this.weatherIcon.Clouds});
+    }
   }
 
   getWeather = async() =>{
@@ -49,9 +84,9 @@ class App extends React.Component{
       celsius:this.calCelsius(response.main.temp),
       temp_max:this.calCelsius(response.main.temp_max),
       temp_min:this.calCelsius(response.main.temp_min),
-      description:response.weather[0].description,
-      icon:this.weatherIcon.Thunderstrom
+      description:response.weather[0].description
     });
+    this.get_WeatherIcon(this.weatherIcon,response.weather[0].id);
   };
  
   state = {}
@@ -59,6 +94,7 @@ class App extends React.Component{
   render(){
     return(
       <div className="App">
+        <Form />
         <Weather 
         city={this.state.city} 
         country={this.state.country} 
