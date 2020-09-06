9a372a94658cf23382b636caead6f5aa907c7054
rashed <rashedfl123@gmail.com>
2020-09-06 20:19:58 +0600

    done weather app

--- src/component/form.component.jsx
+++ src/component/form.component.jsx
@@ -9,14 +9,14 @@ const Form = props =>{
             <div>{props.error?error():null}</div>
             <form onSubmit={props.loadweather}>
                 <div className="row">
-                    <div className="col-md-3 offset-md-3">
-                        <input type="text" className="corm-control" name="city" autoComplete="off" placeholder="City" />
-                    </div>
-                    <div className="col-md-3 mt-md-0 text-md-left">
-                        <button className="btn btn-default text-light bg-dark"><FontAwesomeIcon icon={faSearch} /></button>
-                    </div>
+                    <div className="col-md-4 offset-md-4">
+                        <div className="row">
+                            <input type="text" className="corm-control col-md-9" name="city" autoComplete="off" placeholder="City" />
+                            <button className="btn btn-default text-light bg-dark col-md-2" id="btn" id="btn"><FontAwesomeIcon icon={faSearch} /></button>
+                        </div>                       
+                    </div>                   
                 </div>
-            </form>
+            </form>    
         </div>
     );
 };
